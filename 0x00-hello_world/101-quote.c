5431d985df3b14f07607bd1f77d595ddcf491bd1
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-05 16:42:28 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - error message
+ *
+ * Description: 'print error message'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (0);
+}
