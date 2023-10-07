8da262f52b81c0f637849f5b532838957f93589f
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-06 18:50:35 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - putchar function
+ *
+ * Description: 'using putchar function'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
