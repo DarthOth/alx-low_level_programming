941f8aae19b58c9879c131fbfaa5b38b77ce6ddf
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-06 18:21:25 -0700

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - putchar function
+ *
+ * Description: 'using putchar function'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	return (0);
+}
+                 
