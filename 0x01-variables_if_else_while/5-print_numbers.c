f5c6d397c96270ad16b6931125c5f9a93a558f7a
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-06 18:44:12 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - puts function
+ *
+ * Description: 'using puts function'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		puts(i);
+	}
+	putchar('\n');
+	return (0);
+}
