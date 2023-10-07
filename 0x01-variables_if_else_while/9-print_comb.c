ee5eafd7daee09ca68b7cd229db26b07b7b97323
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-06 19:08:24 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - putchar function
+ *
+ * Description: 'using putchar function'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
