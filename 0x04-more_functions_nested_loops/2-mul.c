3f6899a5ca581c917d48e3a3068d4752253034ae
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-12 20:14:38 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply function.
+ * @a: function parameter
+ * @b: function parameter
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
