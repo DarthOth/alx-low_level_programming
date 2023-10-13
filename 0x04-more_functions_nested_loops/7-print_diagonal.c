34209ba9da2aadb214ebf86a76883a172f10190d
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-12 22:02:07 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_line - print line function.
+ * @n: function parameter
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar('');
+				}
+			}
+		}
+	}
+}
