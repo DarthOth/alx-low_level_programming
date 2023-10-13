05e53f7b10ac51c99ccc1eec6bb41c885713fdae
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-12 20:59:45 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all numbers function.
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
