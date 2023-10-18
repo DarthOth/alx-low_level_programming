d2cc3dfa1e282d64e163af981969bcbdc4b4700b
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-18 05:39:46 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_rev - function print rev
+  * @s: function parameter
+  */
+
+void print_rev(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+		for (i = count - 1; i >= 0; i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+	}
+
+}
