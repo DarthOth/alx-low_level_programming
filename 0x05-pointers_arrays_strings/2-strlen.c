04924703ee662f0330fb82569e933e46eab6a0ca
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-18 05:09:54 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _strlen - function swap
+  * @s: function parameter
+  * Return: count value
+  */
+
+int _strlen(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count)
+}
