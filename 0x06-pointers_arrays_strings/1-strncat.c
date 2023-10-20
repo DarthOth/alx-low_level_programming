f581ac31320519049974a4c0276f3e3ddbdaeee9
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-20 14:42:06 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strncat  - function strncat
+  * @dest: function parameter
+  * @src: function parameter
+  * @n: function parameter
+  * Return: dest
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dest_len = strlen(dest);
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[dest_len + i] = *src;
+		src++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+
+}
