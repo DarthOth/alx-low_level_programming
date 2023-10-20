0223998a5a1cd52230c65bd14b4b04e08793bd21
DarthOth <chahnaouiothmane.2002@gmail.com>
2023-10-20 14:19:11 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strcat  - function strcat
+  * @dest: function parameter
+  * @src: function parameter
+  * Return: dest
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0, src_len = 0, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest_len++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		src_len++;
+	}
+	for (i = 0; i <= srclen; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	return (dest);
+
+}
