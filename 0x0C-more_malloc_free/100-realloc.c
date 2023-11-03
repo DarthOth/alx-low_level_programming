310c690a08c97ea962d4808230b81e65b3b686ca
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-03 05:55:46 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,12 +11,12 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p, oldptr = ptr;
+	char *p, *oldptr = ptr;
 	size_t i, max = new_size;
 
 	if (ptr == NULL)
 	{
-		P = malloc(new_size);
+		p = malloc(new_size);
 		return (p);
 	}
 	else if (new_size == 0)
