f3fa74aa258370b6b249335bdeaf542893af92a3
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-02 17:51:10 -0700

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	j = max - min + 1;
-	a = malloc(sizeof(int) * 1);
+	a = malloc(sizeof(int) * j);
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < j; i++)
