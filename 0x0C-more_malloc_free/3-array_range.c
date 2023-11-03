1fa59a7466a1a8c253e3ef5187380505cc097678
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-02 17:40:15 -0700

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *array_range - function array range
+ * @min: function parameter
+ * @max: function parameter
+ * Return: result
+ */
+
+int *array_range(int min, int max)
+{
+	int *a, i, j;
+
+	if (min > max)
+		return (NULL);
+	j = max - min + 1;
+	a = malloc(sizeof(int) * 1);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+	{
+		a[i] = min;
+		min++;
+	}
+	return (s);
+}
