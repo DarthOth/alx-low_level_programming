cf4586eaa82280acf683fa3098263275ab00154d
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-12 09:47:15 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function name
+ * @argc: function param
+ * @argv: function param
+ * Return: return value
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
