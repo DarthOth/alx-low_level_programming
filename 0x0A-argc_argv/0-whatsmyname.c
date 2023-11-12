58aa658b90a30c6527ed43088c6899b73c91de0e
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-12 09:44:14 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function name
+ * @argc: function param
+ * @argv: function param
+ * Return: return value
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
