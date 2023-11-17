9ed954b0778f6ea20c9376e4762699081a7ce031
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-16 18:19:09 -0800

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - function name
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
