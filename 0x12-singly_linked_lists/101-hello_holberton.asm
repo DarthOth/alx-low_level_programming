14fcfd66e20d038843ea2a9d352b342327885255
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-16 23:36:46 -0800

    task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
-global main
-extern printf
+	global main
+	extern printf
 main:
-mov edit, format
-xor eax, eax
-call printf
-mov eax, 0
-ret
+	mov edit, format
+	xor eax, eax
+	call printf
+	mov eax, 0
+	ret
 format: db 'Hello, Holberton\n', 0
