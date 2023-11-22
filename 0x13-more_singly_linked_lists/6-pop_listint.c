234485e7a8899a78cf2a166b974175cd3282215e
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-21 17:52:10 -0800

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *pop_listint - function name
+ * @head: function param
+ * Return: return value
+ */
+
+int pop_listint(listint_t **head)
+{
+	int num;
+	listint_t *temp;
+
+	if (head || !*head)
+	{
+		return (0);
+	}
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (num);
+}
