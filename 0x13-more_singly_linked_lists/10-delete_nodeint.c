70478ff4e7c6c88d068fa204abb39ab4ed9667a0
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-22 08:02:21 -0800

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (num < index - 1)
 	{
-		if (temp || !(temp->next))
+		if (!temp || !(temp->next))
 		{
 			return (-1);
 		}
