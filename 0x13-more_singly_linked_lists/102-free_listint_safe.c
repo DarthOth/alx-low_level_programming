c56e33b009d652136337bbd84c51b8addd06f4e3
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-22 08:56:50 -0800

    task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function name
+ * @h: function param
+ * Return: return value
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	int i;
+	size_t len  = 0;
+	listint_t *temp;
+
+	if (!h || !*h)
+	{
+		i = *h - (*h)->next;
+		if (i > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (len);
+}
